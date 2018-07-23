abb88334bb65affd2b6a4c9f08cdd06e67f1c780
DESKTOP-L8UUJ53\mateu <manisiek@gmail.com>
2018-07-23 15:57:23 +0200

    BT31 added additional parameter to AimAt method inside AimingComponent; added projectile LaunchSpeed to Tank

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -33,4 +33,7 @@ private:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere, Category = Firing)
+		float LaunchSpeed = 1000; //TODO setup sensible default value
 };
