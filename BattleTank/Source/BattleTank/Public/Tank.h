8686e8c94387bb99c727cc78528b1c920e7a5974
DESKTOP-L8UUJ53\mateu <manisiek@gmail.com>
2018-07-23 12:58:37 +0200

    BT29 added TankAimingComponent for a Tank; added AimAt to the component

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
@@ -18,16 +19,15 @@ public:
 	void AimAt(FVector HitLocation);
 
 protected:
+	UTankAimingComponent * TankAimingComponent = nullptr;
+
+private:	
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-private:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	
-	
 };
