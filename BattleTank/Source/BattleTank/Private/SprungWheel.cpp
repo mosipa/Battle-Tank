0e858ecd765a459305e8a3544c094285c19ecb04
DESKTOP-L8UUJ53\mateu <manisiek@gmail.com>
2018-09-24 17:31:59 +0200

    BT123 added OnHit event to SprungWheel, now applies force when collides; changed it TG

--- BattleTank/Source/BattleTank/Private/SprungWheel.cpp
+++ BattleTank/Source/BattleTank/Private/SprungWheel.cpp
@@ -3,12 +3,14 @@
 #include "SprungWheel.h"
 #include "PhysicsEngine/PhysicsConstraintComponent.h"
 #include "Components/SphereComponent.h"
+#include "Engine/World.h"
 
 // Sets default values
 ASprungWheel::ASprungWheel()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.TickGroup = TG_PostPhysics;
 
 	MassAxleConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("MassAxleConstraint"));
 	SetRootComponent(Cast<USceneComponent>(MassAxleConstraint));
@@ -28,6 +30,9 @@ void ASprungWheel::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	Wheel->SetNotifyRigidBodyCollision(true);
+	Wheel->OnComponentHit.AddDynamic(this, &ASprungWheel::OnHit);
+
 	SetupConstraint();
 }
 
@@ -54,15 +59,29 @@ void ASprungWheel::SetupConstraint()
 	);
 }
 
-void ASprungWheel::AddDrivingForce(float ForceMagnitude)
-{
-	Wheel->AddForce(Axle->GetForwardVector() * ForceMagnitude);
-}
-
 // Called every frame
 void ASprungWheel::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (GetWorld()->TickGroup == TG_PostPhysics)
+	{
+		TotalForceMagnitudeThisFrame = 0;
+	}
 }
 
+void ASprungWheel::AddDrivingForce(float ForceMagnitude)
+{
+	TotalForceMagnitudeThisFrame += ForceMagnitude;
+}
+
+
+void ASprungWheel::ApplyForce()
+{
+	Wheel->AddForce(Axle->GetForwardVector() * TotalForceMagnitudeThisFrame);
+}
+
+void ASprungWheel::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	ApplyForce();
+}
--- BattleTank/Source/BattleTank/Public/SprungWheel.h
+++ BattleTank/Source/BattleTank/Public/SprungWheel.h
@@ -40,5 +40,11 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = Components)
 		UPhysicsConstraintComponent* AxleWheelConstraint = nullptr;
 
+	float TotalForceMagnitudeThisFrame = 0;
+
 	void SetupConstraint();
+	void ApplyForce();
+
+	UFUNCTION()
+		void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 };
